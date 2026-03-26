ef57a2e7bedf70a2bfe29ae979b19a41905f2320
Mohamed-Abdel-Rahem <169306620+Mohamed-Abdel-Rahem@users.noreply.github.com>
2026-03-26 05:49:50 +0200

    feat(mobile): New branch counter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reactivehub/bloc_counter_depi/counter_bloc.dart';
-import 'package:reactivehub/bloc_counter_depi/counter_event.dart';
+
 import 'package:reactivehub/bloc_counter_depi/homeScreen.dart';
 import 'package:reactivehub/bloc_counter_depi/my_observer.dart';
-import 'package:reactivehub/bloc_counter_depi/sconde_screen.dart';
+
 import 'package:reactivehub/bloc_counter_depi/theme_bloc.dart';
-import 'package:reactivehub/bloc_counter_depi/theme_event.dart';
 
 void main() {
   Bloc.observer = MyObserver();
