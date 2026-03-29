43acefeaa43153915f0d3fe45840001cd16abf91
Mohamed-Abdel-Rahem <169306620+Mohamed-Abdel-Rahem@users.noreply.github.com>
2026-03-29 08:40:30 +0200

    add path provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:reactivehub/controllers/bloc/counter_bloc.dart';
 import 'package:reactivehub/controllers/cubit/counter_cubit.dart';
 import 'package:reactivehub/home_page.dart';
 import 'package:reactivehub/models/user_model.dart';
 
-void main() {
-  UserModel user1 = UserModel(id: 45, name: 'Mohamed');
-  UserModel user2 = UserModel(id: 45, name: 'Mohamed');
-  print(user1 == user2);
-  print(user1.hashCode);
-  print(user2.hashCode);
+Future<void> main() async {
 
+WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory:  HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(
     MultiProvider(
       providers: [BlocProvider(create: (context) => CounterBloc())],
