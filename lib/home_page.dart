bb203d367d4874d26a325f7fb6913dfcaa3c61a1
Mohamed-Abdel-Rahem <169306620+Mohamed-Abdel-Rahem@users.noreply.github.com>
2026-03-30 11:46:51 +0200

    add blocConsumer to counter app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reactivehub/controllers/bloc/counter_bloc.dart';
-import 'package:reactivehub/controllers/cubit/counter_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,40 +11,38 @@ class HomePage extends StatelessWidget {
     print('build parent');
 
     return Scaffold(
-      body: BlocListener<CounterBloc, CounterState>(
-        listener: (context, state) {
-          if (state.counter == 5) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Counter is 5'),
-                duration: Duration(seconds: 1),
-              ),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Push to increase or decrease the counter',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              // الـ BlocBuilder دلوقتي بقى "موضعي" فقط حول الجزء المتغير
-              BlocBuilder<CounterBloc, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    '${state.counter}',
-                    style: const TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Push to increase or decrease the counter',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            // الـ BlocBuilder دلوقتي بقى "موضعي" فقط حول الجزء المتغير
+            BlocConsumer<CounterBloc, CounterState>(
+              listener: (context, state) {
+                if (state.counter == 5) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Counter is 5'),
+                      duration: Duration(seconds: 1),
                     ),
                   );
-                },
-              ),
-            ],
-          ),
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  '${state.counter}',
+                  style: const TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
       floatingActionButton: BlocBuilder<CounterBloc, CounterState>(
@@ -57,8 +54,6 @@ class HomePage extends StatelessWidget {
                 heroTag:
                     'add', // متنساش الـ tags عشان الـ Hero error اللي حليناه
                 onPressed: () {
-                
-                  
                   context.read<CounterBloc>().add(IncremetEvent());
                 },
                 child: const Icon(Icons.add),
@@ -66,7 +61,6 @@ class HomePage extends StatelessWidget {
               FloatingActionButton(
                 heroTag: 'sub',
                 onPressed: () {
-                
                   context.read<CounterBloc>().add(DecrementEvent());
                 },
                 child: const Icon(Icons.remove),
